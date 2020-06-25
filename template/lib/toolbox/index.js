f2b9f3fcac9fe149345454bf34b19f0b371f57fc
Adrian Brooks <adrian@adrianbrooks.com>
2020-06-25 00:19:14 -0400

    Extracted lib into swordsmith pakage

--- template/lib/toolbox/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  getLocalIdent: require('./get-local-ident'),
-  DisableWebpackOutputPlugin: require('./disable-webpack-output'),
-  PostrenderHtmlPlugin: require('./postrender-html-plugin'),
-  RenderScriptsPlugin: require('./render-scripts-plugin'),
-}
